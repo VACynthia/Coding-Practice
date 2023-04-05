98b2ed5c808542af9a165b3c5222b15049ebd78a
VACynthia <cynthiavakenya@gmail.com>
2023-04-05 14:06:19 -0700

    User bio

---
+++ C_Variables/Prjct1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that asks user of name and age
+ * Return: 0
+ */
+
+int main(void)
+{
+	char Name[40]; /* Maximum characters that the name can contain */
+	int Age;
+
+	printf("What is your name? ");
+	scanf("%s", Name);
+	printf("How old are you? ");
+	scanf("%d", &Age); /* &Age to display the age as an integer */
+
+	printf("Hello, %s! You are %d years old!\n", Name, Age);
+	return (0);
+}
