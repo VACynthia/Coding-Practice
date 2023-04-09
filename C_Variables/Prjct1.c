6dabafe2bfaea37f6f8c4b2cac48aae515d0536c
VACynthia <cynthiavakenya@gmail.com>
2023-04-08 20:19:48 -0700

    User data

--- C_Variables/Prjct1.c
+++ C_Variables/Prjct1.c
@@ -10,7 +10,7 @@ int main(void)
 	char Name[40]; /* Maximum characters that the name can contain */
 	int Age;
 
-	printf("What is your name? ");
+	printf("What is your name? "); /* %s identifies the response as a script */
 	scanf("%s", Name); /* scanf used to detect user's response */
 	printf("How old are you? ");
 	scanf("%d", &Age); /* &Age to display the age as an integer */
