728b009aabdf79485632c6772681d6c0622ef11a
VACynthia <cynthiavakenya@gmail.com>
2023-04-09 18:41:35 -0700

    The alphabet

--- C_Variables/while.c
+++ C_Variables/while.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	char a = 'a';
-
+	/* while loop execution */
 	while (a <= 'z')
-	{
+	{	/* body of while is in calibraces */
 		printf("The alphabet: %c\n", a);
-		a++;
+		a++; /* increment keeps updating the value of a */
 	}
 	return (0);
 }
