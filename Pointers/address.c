b90807077c89a12cba17cc01558d872189aa66dc
VACynthia <cynthiavakenya@gmail.com>
2023-04-17 04:04:43 -0700

    Address of a variable

---
+++ Pointers/address.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - addresses of variables
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char k;
+	int p;
+
+	printf("Address of variable 'k': %p\n", &k);
+	printf("Address of variable 'p': %p\n", &p);
+
+	return (0);
+}
