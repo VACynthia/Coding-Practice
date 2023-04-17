19e46ec2e76350844fef397f1297c3a31a87be8e
VACynthia <cynthiavakenya@gmail.com>
2023-04-17 04:18:30 -0700

    Address of a pointer

---
+++ Pointers/address_pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing the address of a pointer
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int *y;
+
+	printf("Address of a pointer 'y': %p\n", &y);
+
+	return (0);
+}
